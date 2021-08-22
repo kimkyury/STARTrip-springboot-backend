0aef47f35e7c825851c1c947765c12f80cca9fb5
kimkyury <mandarining0918@gmail.com>
2021-08-22 13:14:48 +0900

    Remove: 서버 실행에 불필요한 파일 제거

--- React_DB/client/src/_reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux"; // Store 공간에 여러가지 Reducer 등이 있음 즉 User 에 대한 state 등등등 있는데 CombineReducer 을 이용해 여러가지 reducer를 합치는 역할
-import user from'./user_reducer';
-
-const rootReducer = combineReducers({
-    user
-})
-
-export default rootReducer;
