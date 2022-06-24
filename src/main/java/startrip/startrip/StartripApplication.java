fa84d73bad4593ef694b080611adcc9b3518db9b
kgyury <mandarining0918@gmail.com>
2022-06-24 15:23:22 +0900

    feat: init setting

--- src/main/java/startrip/startrip/StartripApplication.java
+++ src/main/java/startrip/startrip/StartripApplication.java
@@ -1,4 +1,4 @@
-package com.startrip.backend;
+package startrip.startrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/startrip/startrip/StartripApplicationTests.java
+++ src/test/java/startrip/startrip/StartripApplicationTests.java
@@ -1,4 +1,4 @@
-package com.startrip.backend;
+package startrip.startrip;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
