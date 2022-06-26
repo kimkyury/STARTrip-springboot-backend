1cf5ea482ed85943d7aec287a13fd7e8de94d427
kgyury <mandarining0918@gmail.com>
2022-06-27 00:32:25 +0900

    chore: 잘못 생성된 config파일 삭제

--- src/main/java/STARTrip/WeatherScoreDaemon/config/ScheulerConfig.java
+++
@@ -1,9 +0,0 @@
-package STARTrip.WeatherScoreDaemon.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-public class SchdeulerConfig {
-}
